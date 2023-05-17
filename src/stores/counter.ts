fb80911fff5d45f13b99d495daba6f0bc7a93c77
Hex09AF <lsqhuy@gmail.com>
2023-05-17 21:22:05 +0700

    :sparkles: counter with pinia

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -4,9 +4,15 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
-  function increment() {
+  function inc() {
     count.value++
   }
+  function dec() {
+    count.value--
+  }
+  function asyncInc() {
+    setTimeout(() => count.value++, 1000)
+  }
 
-  return { count, doubleCount, increment }
+  return { count, doubleCount, inc, dec, asyncInc }
 })
---
+++ src/views/CounterView.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <div class="counter">
+    <div>
+      <h1>This is a counter page</h1>
+
+      {{ counter.count }}
+      <button type="button" @click="counter.count++">Sync++</button>
+      <button type="button" @click="counter.dec">Sync--</button>
+      <button type="button" @click="counter.asyncInc">ASync++</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.counter {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+</style>
